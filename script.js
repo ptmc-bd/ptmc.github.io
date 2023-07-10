c649b8c0c9ce3b3828bb9e36280e6ec3cd2fb92a
Pi-Thon Math Club <139137089+ptmc-bd@users.noreply.github.com>
2023-07-10 19:14:15 +0600

    ptmc
    ptmc

---
+++ script.js
@@ -0,0 +1,9 @@
+document.oncontextmenu = new Function("return false");
+// javascript for responsive navigation menu
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
